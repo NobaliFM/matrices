a98446b07522f980ab6393d988f6a07a8d6a4b99
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-08-24 20:31:04 -0600

    Funciona matrices.cpp y .exe

--- matrices.cpp
+++ matrices.cpp
@@ -6,54 +6,49 @@
 #include <math.h>
 
 int matrices, m1, n1, m2, n2;
-
-using namespace std; // Necesario siempre al usar cout o cerr
+using namespace std; // Necesario siempre al usar cout o cin
 int main() {
 	/* Este bloque solicita el número de matrices
 	para declararlas en un ciclo, lo cual aún no se requiere
 	cout<<"Introduce el número de matrices";
 	cin>>matrices;
 	*/
-
 	cout<<"Introduce los valores de m y r para la primera matriz:"<<endl;
 	cout<<"m: ";
 	cin>>m1;
 	cout<<"n: ";
 	cin>>n1;
-
 	cout<<endl;
-
 	cout<<"Introduce los valores de m y r para la segunda matriz:"<<endl;
 	cout<<"m: ";
 	cin>>m2;
 	cout<<"n: ";
 	cin>>n2;
-
 	cout<<endl;
-
 	if(n1 != m2) {
 		cout<<"El producto de las matrices está indefinido."<<endl;
 		exit;
 	}
-
-
 	int matriz1[m1][n1];
-
 	cout<<"Ingresa las datos de la matriz 1:"<<endl;
-
 	for(int j = 0; j<n1; j++) {
 		for(int i = 0; i < m1; i++) {
 			cout<<"Ingresa m"<<i<<"j"<<j<<": ";
 			cin>>matriz1[i][j];
-			cout<<endl;a
+			cout<<endl;
 		}
 	}
-
+	for (int i = 0; i<m1; i++) {
+		cout<<"   ";
+		for (int j = 0; j<n1; j++) {
+			//cout<<"Dato ["<<i<<"]["<<j<<"]: "<<matriz1[i][j]<<endl;
+			cout<<matriz1[i][j]<<"   ";
+		}
+		cout<<endl;
+	}
 
 	int matriz2[m2][n2];
-
 	cout<<"Ingresa las datos de la matriz 2:"<<endl;
-
 	for(int j = 0; j<n2; j++) {
 		for(int i = 0; i < m2; i++) {
 			cout<<"Ingresa m"<<i<<"j"<<j<<": ";
@@ -61,19 +56,42 @@ int main() {
 			cout<<endl;
 		}
 	}
-
+	for (int i = 0; i<m2; i++) {
+		cout<<"   ";
+		for (int j = 0; j<n2; j++) {
+			//cout<<"Dato ["<<i<<"]["<<j<<"]: "<<matriz1[i][j]<<endl;
+			cout<<matriz2[i][j]<<"   ";
+		}
+		cout<<endl;
+	}
 	int matriz3[m1][n2];
 	int valor;
-	for (int i=0; i<m1; i++) {
-		// Sumar 3 números calculados individualmente (productos)
-		for(int j=0; j<n2; j++) {
-			valor = 0;
-			for(int k=0; k<m1; k++){
-				for (int l = 0; l<n2; l++){
-					valor = ((matriz1[i][])*(matriz2[][]))
-				}
+	// Código para calcular las direcciones de matriz 3
+	for(int a = 0; a<m1; a++) { // Iterador a
+		// Aquí se harán los cálculos para los datos de m iterado (empieza en m: a = 0)
+		for(int b = 0; b<n2; b++) { // Iterador b
+			// Aquí se harán los cálculos para los datos de m iterado (empieza en n: b = 0)
+			int acumulador = 0;
+			for(int c = 0; c<m1; c++) { // Iterador c
+				// Aquí se harán los cálculos para los datos de m iterado (empieza en l: c = 0)
+				int sumador = 0;
+				sumador = ( ( matriz1[a][c] ) * ( matriz2[c][b] ) );
+				acumulador = (acumulador + sumador);
+				sumador = 0;
 			}
-			
+			matriz3[a][b] = acumulador;
+			acumulador = 0;
 		}
+	}
+
+	cout<<endl;
+	
+	for (int i = 0; i<m1; i++) {
+		cout<<"   ";
+		for (int j = 0; j<n2; j++) {
+			//cout<<"Dato ["<<i<<"]["<<j<<"]: "<<matriz1[i][j]<<endl;
+			cout<<matriz3[i][j]<<"   ";
+		}
+		cout<<endl;
 	}
 }
--- multiplicacion_de_matrices.c
+++ multiplicacion_de_matrices.c
@@ -1,64 +1,75 @@
 #include <stdio.h>
-int save_matrix(int columns, int rows, int matrix[columns][rows]){
+int save_matrix(int columns, int rows, int matrix[columns][rows]) {
 	int l,p;
-	 for (l=1;l<=columns;l++){
-        for (p=1; p<=rows;p++){
-            printf("[%i][%i]: ",l,p);
-            scanf("%i",& matrix[l][p]);
-        };
-    };
-	return 0;
-	};
-
-int matrix_display(int nColums, int nRows, int matrix[nColums][nRows]){
-    int l,p;
-    for (l=1;l<=nColums;l++){
-        for (p=1; p<=nRows;p++){
-            printf("%5i",matrix[l][p]);
-        };
-         printf("\n");
-    };
-    return 0;
-  };
-  
-int main (void){
-	 int i,j,k,o,num;
-	 printf("MULTIPLICADOR DE MATRICES\n\n");
-	 printf("Tamaño de la matrix numero 1: \n");
-	 printf("Filas:\n"); scanf("%i", &i); 
-	 printf("columnas:\n"); scanf("%i", &j);
-     printf("Tamaño de la matrix numero 2: \n");
-     printf("filas:\n"); scanf("%i", &k); 
-	 printf("columnas:\n"); scanf("%i", &o);
-     //por si no se pueden multiplicar :)
-      while (j != k){
-		  printf("No se pueden multiplicar las matrices. Intentalo de nevo\n");
-		  printf("Tamaño de la matrix numero 1: \n");
-		 printf("Filas:\n");scanf("%i", &i); printf("Columnas: \n");  scanf("%i", &j);
-		  printf("Tamaño de la matrix numero 2: \n");
-		  printf("Filas:\n");scanf("%i", &k); printf("Columnas: \n");  scanf("%i", &o);
+	for (l=1; l<=columns; l++) {
+		for (p=1; p<=rows; p++) {
+			printf("[%i][%i]: ",l,p);
+			scanf("%i",& matrix[l][p]);
 		};
-		 int matrix[i][j];
-		 int matriz[k][o];
-		 int matriz_resp[i][o];
-    //asignar datos a las matrices 
-    printf("Datos de la matriz 1:\n");
+	};
+	return 0;
+};
+
+int matrix_display(int nColums, int nRows, int matrix[nColums][nRows]) {
+	int l,p;
+	for (l=1; l<=nColums; l++) {
+		for (p=1; p<=nRows; p++) {
+			printf("%5i",matrix[l][p]);
+		};
+		
+		printf("\n");
+	};
+	return 0;
+};
+
+int main (void) {
+	int i,j,k,o,num;
+	printf("MULTIPLICADOR DE MATRICES\n\n");
+	printf("Tamaño de la matrix numero 1: \n");
+	printf("Filas:\n");
+	scanf("%i", &i);
+	printf("columnas:\n");
+	scanf("%i", &j);
+	printf("Tamaño de la matrix numero 2: \n");
+	printf("filas:\n");
+	scanf("%i", &k);
+	printf("columnas:\n");
+	scanf("%i", &o);
+	//por si no se pueden multiplicar :)
+	while (j != k) {
+		printf("No se pueden multiplicar las matrices. Intentalo de nevo\n");
+		printf("Tamaño de la matrix numero 1: \n");
+		printf("Filas:\n");
+		scanf("%i", &i);
+		printf("Columnas: \n");
+		scanf("%i", &j);
+		printf("Tamaño de la matrix numero 2: \n");
+		printf("Filas:\n");
+		scanf("%i", &k);
+		printf("Columnas: \n");
+		scanf("%i", &o);
+	};
+	int matrix[i][j];
+	int matriz[k][o];
+	int matriz_resp[i][o];
+	//asignar datos a las matrices
+	printf("Datos de la matriz 1:\n");
 	save_matrix(i,j,matrix);
-    printf("Datos de la matriz 2:\n");
-    save_matrix(k,o, matriz);
-    //imprimir matrices 
-    printf("Primera matriz\n");
-    matrix_display(i,j,matrix);
-    printf("segunda matriz\n");
-    matrix_display(k,o,matriz);
-    //multiplicacion de matrices
-    num=1;
-    /*
-    for (i=1;i=j; i++){
+	printf("Datos de la matriz 2:\n");
+	save_matrix(k,o, matriz);
+	//imprimir matrices
+	printf("Primera matriz\n");
+	matrix_display(i,j,matrix);
+	printf("segunda matriz\n");
+	matrix_display(k,o,matriz);
+	//multiplicacion de matrices
+	num=1;
+	/*
+	for (i=1;i=j; i++){
 		matrix[]matrix[num][i]*matriz[i][num]
 		num++;
 		};
 		* */
-    
-return 0;
+
+	return 0;
 };
